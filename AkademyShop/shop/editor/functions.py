abb293363415f0519dff0ed300188dc746ad9f64
Макар Павлович <106832520+mokinprokin@users.noreply.github.com>
2024-09-11 16:54:31 +0600

    Delete AkademyShop/shop directory

--- AkademyShop/shop/editor/functions.py
+++
@@ -1,7 +0,0 @@
-
-from django.core.files.storage import default_storage
-
-def UploadFileToDB(f):
-    with default_storage.open('editor/static/images/' + f.name, 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
--- AkademyShop/shop/editor/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class Item(models.Model):
-    name=models.TextField()
-    count=models.IntegerField()
-    cost=models.IntegerField()
-    description=models.TextField()
-    fileName=models.FileField()
-# Create your models here.
